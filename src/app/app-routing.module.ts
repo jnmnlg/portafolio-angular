c1b6a8e4dd9241faf62dbadc919efd426b109c02
Juan Manuel <jnmnlg@gmail.com>
2020-05-27 14:19:08 -0300

    JMG20200527.1419

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,41 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ItemComponent } from './pages/item/item.component';
 
 
-const routes: Routes = [];
+
+
+// tslint:disable-next-line: variable-name
+const app_routes: Routes = [
+  {path: '', component: PortafolioComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'item', component: ItemComponent},
+  {path: '**', pathMatch: 'full', redirectTo: ''}
+];
+
+
+
+
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(app_routes)
+  ],
+    exports: [
+      RouterModule
+    ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+}
+
+
+
+
+
+
+
+
